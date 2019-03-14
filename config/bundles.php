108146096eae42d63e52bb18a859ef86ef30688b
Trayan Ivanov <tivanov@mailjet.com>
2019-03-14 18:30:43 +0200

    Sluggable study

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -28,7 +28,6 @@ class ArticleFixtures extends BaseFixture
     {
         $this->createMany(Article::class, 10, function (Article $article, $count) {
             $article->setTitle($this->faker->randomElement(self::$articleTitles))
-                ->setSlug($this->faker->slug)
                 ->setContent('Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore.');
 
             if ($this->faker->boolean(70)) {
