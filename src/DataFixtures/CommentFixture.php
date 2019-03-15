de3060a54ed881c7c901ad1715912b5f99a32980
Trayan Ivanov <tivanov@mailjet.com>
2019-03-15 12:40:43 +0200

    Extra lazy study

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -12,10 +12,7 @@ class CommentFixture extends BaseFixture implements DependentFixtureInterface
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(Comment::class, 100, function (Comment $comment) {
-            $comment->setContent(
-                $this->faker->boolean() ? $this->faker->paragraph : $this->faker->sentences(2, true)
-            );
-
+            $comment->setContent($this->faker->sentences(2, true));
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
 
